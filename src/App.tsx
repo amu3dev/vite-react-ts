9ac71db8045e8c14589b1199ac62ca1f89d82f15
Ahmed Yosry <amu3dev@gmail.com>
2023-03-27 12:34:51 +0200

    - Added Like Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 import { useState } from "react";
+import NewButton from "./components/NewButton/NewButton";
+import Like from "./components/Like";
 
 const App = () => {
   const cities = ["New York", "London", "Toronto", "Sydney", "Mumbai"];
@@ -9,15 +11,24 @@ const App = () => {
   const handleSlectedItem = (index: number, name: string) => {
     console.log(name, index);
   };
+  const [visableDanger, setVisableDanger] = useState(true);
+  const [visableSecondry, setVisableSecondry] = useState(true);
+
   const [visableAlert, setVisableAlert] = useState(true);
 
   return (
     <div>
-      {visableAlert && (
+      <Like
+        onClick={() => {
+          console.log("clicked");
+        }}
+      />
+
+      {visableDanger && (
         <Alert
           type="danger"
           onAlertClick={() => {
-            setVisableAlert(!visableAlert);
+            setVisableDanger(!visableDanger);
           }}>
           This is a danger alert
         </Alert>
@@ -27,23 +38,73 @@ const App = () => {
         type="danger"
         onClick={() => {
           console.log("Clicked");
-          setVisableAlert(!visableAlert);
+          setVisableDanger(!visableDanger);
         }}>
         Danger
       </Button>
-      {/* <ListGroup
-          cities={cities}
-          heading={heading}
-          onSelectedItem={handleSlectedItem}
-        /> */}
-
-      {/* <Alert type="secondary">This is a secondary alert</Alert>
-      <Alert type="success">This is a success alert</Alert>
-      <Alert type="danger">This is a danger alert</Alert>
-      <Alert type="warning">This is a warning alert</Alert>
-      <Alert type="info">This is a info alert</Alert>
-      <Alert type="light">This is a light alert</Alert>
-      <Alert type="dark">This is a dark alert</Alert> */}
+      <ListGroup
+        items={cities}
+        heading={heading}
+        onSelectedItem={handleSlectedItem}
+      />
+      {visableSecondry && (
+        <Alert
+          type="secondary"
+          onAlertClick={() => {
+            setVisableSecondry(!visableSecondry);
+          }}>
+          This is a secondary alert
+        </Alert>
+      )}
+      {visableAlert && (
+        <>
+          <Alert
+            type="success"
+            onAlertClick={() => {
+              setVisableAlert(!visableAlert);
+            }}>
+            This is a success alert
+          </Alert>
+          <Alert
+            type="danger"
+            onAlertClick={() => {
+              setVisableAlert(!visableAlert);
+            }}>
+            This is a danger alert
+          </Alert>
+          <Alert
+            type="warning"
+            onAlertClick={() => {
+              setVisableAlert(!visableAlert);
+            }}>
+            This is a warning alert
+          </Alert>
+          <Alert
+            type="info"
+            onAlertClick={() => {
+              setVisableAlert(!visableAlert);
+            }}>
+            This is a info alert
+          </Alert>
+          <Alert
+            type="light"
+            onAlertClick={() => {
+              setVisableAlert(!visableAlert);
+            }}>
+            This is a light alert
+          </Alert>
+          <Alert
+            type="dark"
+            onAlertClick={() => {
+              setVisableAlert(!visableAlert);
+            }}>
+            This is a dark alert
+          </Alert>
+          <NewButton type="info" onClick={() => console.log("NewButton")}>
+            New Button
+          </NewButton>
+        </>
+      )}
     </div>
   );
 };
